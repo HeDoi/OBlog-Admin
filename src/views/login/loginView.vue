c8abd634aa0373cfbb1867a3b9406074b0781615
hedoi <1784516575@qq.com>
2022-05-21 19:05:52 +0800

    fix: 修复登陆密码框展示

--- src/views/login/loginView.vue
+++ src/views/login/loginView.vue
@@ -6,26 +6,26 @@
       <div class="title-container">
         <h3 class="title">用户登录</h3>
       </div>
-      <el-form class="login-form">
-        <el-form-item>
+      <el-form class="login-form" :model="loginFrom" :rules="loginRules">
+        <el-form-item prop="username">
           <span class="svg-container">
             <i class="iconfont icon-user"></i>
           </span>
-          <el-input name="username" type="text">
+          <el-input name="username" type="text" v-model="loginFrom.username">
             <template #suffix>
             </template>
           </el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="password">
           <span class="svg-container">
             <i class="iconfont icon-password"></i>
           </span>
-          <el-input name="password">
+          <el-input name="password" :type="passwordShow?'text':'password'" v-model="loginFrom.password">
             <template #suffix>
             </template>
           </el-input>
-          <span class="show-pwd">
-            <i class="iconfont icon-view-off"></i>
+          <span class="show-pwd" @click="onChangePasswordShow">
+            <i class="iconfont" :class="passwordShow?'icon-view':'icon-view-off'"></i>
           </span>
         </el-form-item>
         <el-button type="primary" size="default" style="width: 100%">登录</el-button>
@@ -35,11 +35,29 @@
 </template>
 
 <script setup>
-import {} from 'vue'
+import { ref } from 'vue'
+const loginFrom = ref({
+  username: 'admin',
+  password: '123456'
+})
+
+const loginRules = {
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '密码应大于五位', trigger: 'blur', min: 6 }
+  ]
+}
+
+const passwordShow = ref(false)
+const onChangePasswordShow = () => {
+  passwordShow.value = !passwordShow.value
+}
+
 </script>
 
 <style lang="scss" scoped>
-@import url(//at.alicdn.com/t/font_3418537_ltqyl35tn4.css);
 @import '@/style/login/login.scss';
 .iconfont {
   width: 1em;
