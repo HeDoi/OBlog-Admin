605690d6bebb7d164dea65739f8953b7dbb76b28
hedoi <1784516575@qq.com>
2022-05-25 21:13:16 +0800

    feat: navBar开发

--- src/views/layout/components/navBar.vue
+++ src/views/layout/components/navBar.vue
@@ -1,10 +1,59 @@
 <template>
-  <div></div>
+  <div class="navBar">
+    <div class="right-menu">
+      <el-dropdown class="avatar-wrapper" trigger="click">
+        <div class="avatar-container">
+          <el-avatar shape="square" :size="40" :src="store.getters.userInfo.avatar"></el-avatar>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu class="user-dropdown">
+            <router-link to="/">
+              <el-dropdown-item>首页</el-dropdown-item>
+            </router-link>
+            <a href="/">
+              <el-dropdown-item>课程首页</el-dropdown-item>
+            </a>
+            <el-dropdown-item divided>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import {} from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
 </script>
 
 <style lang="scss" scoped>
+@import '@/style/variables.scss';
+.navBar {
+  height: $navBarHeight;
+  overflow: hidden;
+  position: relative;
+  background-color: #fff;
+  box-shadow: 0 1px 4px rgb(0, 21, 41, 0.08);
+
+  .right-menu {
+    display: flex;
+    align-items: center;
+    float: right;
+
+    :deep(.avatar-wrapper) {
+      cursor: pointer;
+
+      .avatar-container {
+        margin-top: calc((#{$navBarHeight} - 40px) / 2);
+        position: relative;
+
+        .el-avatar {
+          --el-avatar-border-radius: none;
+          --el-avatar-bg-color: none;
+        }
+      }
+    }
+  }
+}
 </style>
--- src/views/layout/indexView.vue
+++ src/views/layout/indexView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="app-wrapper">
-    <side-bar class="sidebar-container"></side-bar>
-    <div class="main-container">
+    <side-bar class="sideBar-container"></side-bar>
+    <div class="app-container">
       <div class="fixed-header">
-        <nav-bar class="navbar-container"></nav-bar>
+        <nav-bar class="navBar-container"></nav-bar>
       </div>
       <app-main class="main-container"></app-main>
     </div>
@@ -23,13 +23,13 @@ import AppMain from './components/appMain.vue'
   width: 100%;
   height: 100%;
 
-  .sidebar-container {
+  .sideBar-container {
     float: left;
     width: $sideBarWidth;
     height: 100%;
     background-color: #666;
   }
-  .main-container {
+  .app-container {
     float: right;
     width: calc(100% - #{$sideBarWidth});
     height: 100%;
